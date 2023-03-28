f9162688d569929c90eac7417ca65917de5a7c61
orhan.metin@etiya.com <orhan.metin@etiya.com>
2023-03-28 11:27:13 +0300

    Angular CLI
    component-class-pipe-module-build-start

--- FirstProject/src/app/app.module.ts
+++ FirstProject/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponent } from './my/my.component';
+import { MyTestComponent } from './my-test/my-test.component';
+import { MypipePipe } from './mypipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponent,
+    MyTestComponent,
+    MypipePipe
   ],
   imports: [
     BrowserModule,
---
+++ FirstProject/src/app/my-test/my-test.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-test',
+  templateUrl: './my-test.component.html',
+  styleUrls: ['./my-test.component.css']
+})
+
+export class MyTestComponent implements OnInit {
+deger: string = "";
+
+  
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+}
