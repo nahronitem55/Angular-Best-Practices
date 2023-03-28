3354278eff361894c02ed79ecfb4f22bfe99e5a9
orhan.metin@etiya.com <orhan.metin@etiya.com>
2023-03-28 12:42:58 +0300

    TrackBy Example

--- FirstProject/src/app/app.component.ts
+++ FirstProject/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'FirstProject';
+
+  users: any[] = [];
+
+  ngOnDestroy(): void {
+
+  }
+  ngOnInit(): void {
+    this.httpClient.get<any[]>("https://jsonplaceholder.typicode.com/users").subscribe(data => {
+      this.users = data;
+    });
+  }
+
+  addUser() {
+    this.httpClient.get<any[]>("https://jsonplaceholder.typicode.com/users").subscribe(data => {
+      this.users = data;
+
+      this.users.push({ name: "Orhan Metin" });
+    });
+  }
+
+  trackByFn(index: number,item: any){
+    return index;
+  }
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
 }
--- FirstProject/src/app/app.module.ts
+++ FirstProject/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MyComponent } from './my/my.component';
 import { MyTestComponent } from './my-test/my-test.component';
 import { MypipePipe } from './mypipe.pipe';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MypipePipe } from './mypipe.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
