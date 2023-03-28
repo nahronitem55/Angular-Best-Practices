3cf493e5fa8b4392c27702f1cbcc2b3a3aee497e
orhan.metin@etiya.com <orhan.metin@etiya.com>
2023-03-28 15:24:26 +0300

    Component to takeUntil

--- FirstProject/src/app/b/b.component.ts
+++ FirstProject/src/app/b/b.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {  Observable, delay, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-b',
   templateUrl: './b.component.html',
   styleUrls: ['./b.component.css']
 })
-export class BComponent implements OnInit {
+export class BComponent implements OnInit, OnDestroy {
 
-  constructor() { }
 
+  constructor(private httpClient: HttpClient) { }
+  ngOnDestroy(): void {
+    this.unscribeSubject.next(this.users);
+    this.unscribeSubject.complete();
+  }
+  users: any[] = [];
+  unscribeSubject = new Subject();
   ngOnInit(): void {
+    this.httpClient.get<any[]>("https://jsonplaceholder.typicode.com/users")
+      .pipe(delay(5000),takeUntil(this.unscribeSubject))
+      .subscribe(data => {
+      this.users = data;
+    });
   }
 
 }
