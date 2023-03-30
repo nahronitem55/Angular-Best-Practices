857d9f19a1a3f556e7faabca0faacff329553be8
orhan.metin@etiya.com <orhan.metin@etiya.com>
2023-03-30 14:15:12 +0300

    const- let- temp logic

--- FirstProject/src/app/a/a.component.ts
+++ FirstProject/src/app/a/a.component.ts
@@ -9,12 +9,18 @@ import { delay } from "rxjs";
   styleUrls: ['./a.component.css']
 })
 export class AComponent implements OnInit {
+  controlTest: boolean = false;
 
   constructor(private httpClient: HttpClient) { }
 
   users: Observable<any[]> | undefined;
   ngOnInit(): void {
+    const testAComp = "A const";
+    let testACompLet = "A let" 
     this.users = this.httpClient.get<any[]>("https://jsonplaceholder.typicode.com/users").pipe(delay(5000));
+    if(this.users != null){
+      this.controlTest = true;
+    }
   }
 
 }
